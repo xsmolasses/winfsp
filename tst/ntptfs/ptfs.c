a1be6bf04ce468d1163d4ccb3bbeac669f27037f
xsmolasses <87760655+xsmolasses@users.noreply.github.com>
2024-06-17 19:10:49 +0800

    Update ptfs.c

--- tst/ntptfs/ptfs.c
+++ tst/ntptfs/ptfs.c
@@ -956,17 +956,18 @@ static NTSTATUS GetReparsePoint(FSP_FILE_SYSTEM *FileSystem, // xsmolasses earma
     PVOID FileContext,
     PWSTR FileName, PVOID Buffer, PSIZE_T PSize)
 {
+    PTFS *Ptfs = FileSystemContext; // xsmolasses
+    HANDLE Handle = FileContextHandle;
+    ULONG BytesTransferred;
+    NTSTATUS Result;
+
 // xsmolasses
-    PTFS *Ptfs = FileSystemContext;
     if (!(Ptfs->FsAttributeMask & PtfsReparsePoints))
     {
         Result = STATUS_NOT_A_REPARSE_POINT;
         goto exit;
     }
 //
-    HANDLE Handle = FileContextHandle;
-    ULONG BytesTransferred;
-    NTSTATUS Result;
 
     Result = LfsFsControlFile(
         Handle,
